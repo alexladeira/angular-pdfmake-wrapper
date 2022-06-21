ec380d93b2f8d4cd6de2d69a608935104b5189d6
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2022-06-21 15:33:53 -0300

    adding pdfmake and pdfmake-wrapper dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PdfMakeWrapper } from 'pdfmake-wrapper';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts'; // fonts provided for pdfmake
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pdfmake-wrapper';
+  pdf = new PdfMakeWrapper();
+
+  ngOnInit(): void {
+    PdfMakeWrapper.setFonts(pdfFonts);
+  }
+
+  click = () => {
+    this.pdf.add('Hello world!');
+
+    this.pdf.create().download();
+  };
 }
